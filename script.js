76791f8ed442e164815f01e3e2863940645dd8bc
Arbab Ahamad <ahmadarbab.456@gmail.com>
2026-07-27 19:12:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,10 +6,57 @@ const scores = document.getElementById("scores");
 // Save score to Local Storage
 function saveScore() {
   // complete the code here
+
+	const name = nameInput.value.trim();
+	const score = scoreInput.value.trim();
+
+	let highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+	highScores.push({
+		name: name,
+		score: score
+	});
+
+	localStorage.setItem("highScores", JSON.stringify(highScores));
+
+	nameInput.value = "";
+	scoreInput.value = "";
+	
   showScores();
 }
 
 // Show scores in div
 function showScores() {
   // complete the code
+
+	const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+	if(highScores.length === 0) {
+		scores.innerHTML = "No scores yet";
+		return;
+	}
+
+	let table = `
+		<table border="1">
+			<tr>
+				<th>Name</th>
+				<th>Score</th>
+			</tr>
+	`;
+
+	highScores.forEach((item) => {
+		table += `
+			<tr>
+				<td>${item.name}</td>
+				<td>${item.score}</td>
+			</tr>
+		`;
+	});
+
+	table += "</table>"
+
+	scores.innerHTML = table;
 }
+
+
+
